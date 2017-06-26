ba849802ef3ec81a0250425a1ef0ef5d520724f0
Kumaresan <kumaresh876@gmail.com>
2017-06-26 11:22:09 +0530

    First Web Commit

---
+++ footer.php
@@ -0,0 +1,32 @@
+        <footer>
+            <div class="container">
+                <div class="row">
+
+                    <!-- Footer Bottom -->
+                    <div class="trd-footer-bottom">
+                        <div class="trd-footer-menu">
+                            <ul>
+                                <li>
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="about.html">About</a>
+                                </li>
+                                <li>
+                                    <a href="services.html">Blog</a>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="trd-copyright-info">
+                            <p>Copyright &copy; 2017<a href="#" class="trd-author-link"></a></p>
+                        </div>
+                    </div>
+                    <!-- End -->
+                </div>
+            </div>
+            <!-- End -->
+        </footer>
---
+++ header.php
@@ -0,0 +1,93 @@
+ <!-- HEADER -->
+        <header class="trd-header">
+            <!-- Topbar -->
+            <div class="trd-header-topbar">
+                <div class="container">
+                    <div class="row">
+                        <!-- contact info -->
+                        <div class="trd-contact-infos">
+                            <ul>
+                                <li class="trd-header-info-phn">+91-9543449907</li>
+                                <li class="trd-header-info-location">2nd street, Bethaniyapuram, Madurai</li>
+                         
+                            </ul>
+                        </div>
+                        <!-- End -->
+
+                        <!-- Social Links -->
+                        <div class="trd-social-links">
+                            <ul>
+                                <li class="trd-fb-icon">
+                                    <a href="#">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li class="trd-twitter-icon">
+                                    <a href="#">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li class="trd-behance-icon">
+                                    <a href="#">
+                                        <i class="fa fa-behance"></i>
+                                    </a>
+                                </li>
+              
+                            </ul>
+                        </div>
+                        <!-- End -->
+                    </div>
+                </div>
+            </div>
+            <!-- End -->
+
+            <!-- Bottombar -->
+            <div class="trd-header-bottombar">
+                <!-- Navigation Menu start-->
+                <nav class="navbar trd-main-menu" role="navigation">
+                  <div class="container">
+                    <div class="row">
+                      <!-- Navbar Toggle -->
+                      <div class="navbar-header">
+                          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                          </button>
+
+                          <!-- Logo -->
+                          <a class="navbar-brand" href="index.html"><img class="logo" src="images/logo.png" alt="LOGO"></a>
+                      </div>
+                      <!-- Navbar Toggle End -->
+
+                        <!-- navbar-collapse start-->
+                        <div id="nav-menu" class="navbar-collapse trd-menu-wrapper collapse" role="navigation">
+                            <!-- Menu -->
+                            <ul class="nav navbar-nav trd-menus">
+                                <li class="active">
+                                    <a href="index.html">Home</a>
+                                    
+                                </li>
+                                <li>
+                                    <a href="about.html">About</a>
+                                </li>
+                                <li>
+                                    <a href="services.html">Services</a>
+                                    
+                                </li>
+                                
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                            <!-- End -->
+                        </div>
+                        <!-- navbar-collapse end-->
+                    </div>
+                  </div>
+                </nav>
+                <!-- Navigation Menu end-->
+            </div>
+            <!-- End -->
+        </header>
+        <!-- HEADER END -->
